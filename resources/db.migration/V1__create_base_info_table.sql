a5b27eff74eb60795ed6ff6ba336c87bc75ecc80
JeonJaeHyeong <tinywind@linkbiz.kr>
2015-09-01 17:04:37 +0900

    first commit

---
+++ resources/db.migration/V1__create_base_info_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "member"
+(
+  id       BIGSERIAL PRIMARY KEY      NOT NULL,
+  username VARCHAR(256)               NOT NULL,
+  age      INTEGER
+);
---
+++ src/my/demo/Test.kt
@@ -0,0 +1,13 @@
+package my.demo
+
+import javax.persistence.EntityManager
+
+/**
+ * Created by tinywind on 2015-09-01.
+ */
+public class Test {
+
+    override fun toString(): String {
+        return super.toString()
+    }
+}
